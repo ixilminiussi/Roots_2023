1b5e7163983eb703d4d87570222bffb1352d5ada
ixil.miniussi <ixil.miniussi@outlook.fr>
2023-02-04 11:21:30 +0000

    movement

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+export var tilesize = 64
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,8 +12,35 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_just_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_just_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_just_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_just_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * tilesize
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+
+	position += velocity
+	#position.x = clamp(position.x, 0, screen_size.x)
+	#position.y = clamp(position.y, 0, screen_size.y)
+
+	#if velocity.x != 0:
+	#	$AnimatedSprite.animation = "carrot"
+	#	$AnimatedSprite.flip_v = false
+	#	$AnimatedSprite.flip_h = velocity.x < 0
+	#elif velocity.y != 0:
+	#	$AnimatedSprite.animation = "up"
+	#	$AnimatedSprite.flip_v = velocity.y > 0
+
 
 func start(pos):
 	position = pos
